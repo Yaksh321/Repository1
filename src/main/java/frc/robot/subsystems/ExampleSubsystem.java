cb486a21ddf52c452290d2c78580863e0316e30e
Yaksh321 <Yaks@yaksh.com>
2024-11-20 17:53:55 -0600

    bhvgjhugik

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,9 +4,12 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ExampleSubsystem extends SubsystemBase {
@@ -15,17 +18,26 @@ public class ExampleSubsystem extends SubsystemBase {
   CANSparkMax motorFront;
   CANSparkMax motorBack;
   int neGus;
+  PIDController PIDZ;
+  RelativeEncoder EncoderZ;
+
+  double inputPID;
+  double outputPID;
   
   public ExampleSubsystem() {
-    motorOne = new CANSparkMax(14, MotorType.kBrushless);
-    motorFront = new CANSparkMax(11, MotorType.kBrushless);
+    motorOne = new CANSparkMax(16, MotorType.kBrushless);
+    EncoderZ = motorOne.getEncoder();
+    EncoderZ.setPosition(0);
  
     neGus = -1;
+
+    PIDZ = new PIDController(0,0,0);
   }
 
     public void runMotorOne(double speed) {
-      motorOne.set(speed);
+      motorOne.set(PIDZ.calculate(EncoderZ.getPosition(), 0));
 
+      System.out.println(EncoderZ.getPosition());
     }
     
     public void stopMotorOne() {
@@ -39,13 +51,17 @@ public class ExampleSubsystem extends SubsystemBase {
 
 public void runShooterMotors (double speed) {
 
-  motorOne.set(speed * neGus);
-
-  motorFront.set(speed * neGus);
-
-  neGus = neGus * -1;
-
-
 }
+
+
+
+
+   
+
+  
+  
+  
+  
+  
   }
 
